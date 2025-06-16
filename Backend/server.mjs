6ad4417d3a61c7aca8d15980f260a8e82ba8a6a1
Reeon_Lance <tobia166@gmail.com>
2025-06-16 23:07:49 +0800

    Send Email for OTP Complete. Ongoing OTP verification

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -1,3 +1,4 @@
+import nodemailer from 'nodemailer';
 import express from 'express';
 import cors from 'cors';
 import { writeRegisterData } from './Firebase.mjs';
@@ -8,8 +9,22 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+const otpStore = {};
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'tobia166@gmail.com',
+        pass: 'mtyh gfrd noaf aejh'
+    }
+});
+
+function generateOTP() {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+}
+
 // Register
-app.post('/save-user', async (req, res) => {
+app.post('/register', async (req, res) => {
     try {
         const { email, firstName, lastName, password, role } = req.body;
         if (!email || !firstName || !lastName || !password) {
@@ -17,14 +32,29 @@ app.post('/save-user', async (req, res) => {
         }
         const user = await writeRegisterData(email, firstName, lastName, password, role);
         console.log('User Registered:', user);
+
+        const otp = generateOTP();
+        otpStore[email] = { otp, expires: Date.now() + 3 * 60 * 1000 };
+
+        try {
+            await transporter.sendMail({
+                from: '"Infinity" <tobia166@gmail.com>',
+                to: email,
+                subject: 'OTP Code',
+                text: `Your OTP Code ${otp}`
+            });
+        } catch (error) {
+            console.log('Error sending email:', error);
+            return res.status(500).json({ success: false, error: 'Failed to send Email' });
+        }
+
         return res.json({ success: true });
     } catch (error) {
         console.log('User not registered:', error);
-        res.status(500).json({ success: false, error: error.message });
+        return res.status(500).json({ success: false, error: error.message });
     }
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-
